544baf8668af9603591aac0c56d9c78c4d99f5b1
Prabesh Bista <83439329+PrabeshPP@users.noreply.github.com>
2024-04-15 01:17:44 -0400

    [added] model for the movie

--- MVCMovie/Controllers/HelloWorldController.cs
+++ MVCMovie/Controllers/HelloWorldController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace MVCMovie;
 
-public class HelloWorldController
+public class HelloWorldController:Controller
 {
-    public string Index(){
-        return "This is my default action...";
+    public IActionResult Index(){
+        return View();
     }
 
-    public string Welcome(string name, int ID= 1){
-        return HtmlEncoder.Default.Encode($"Hello {name}, Id:{ID}");
+    public IActionResult Welcome(string name, int numTimes= 1){
+        
+        //ViewData is dynamic dictionary 
+        ViewData["Message"] = "Hello" + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
---
+++ MVCMovie/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCMovie;
+
+public class Movie
+{   
+    [Key]
+    public int Id{get;set;}
+    public string? Title{get;set;}
+    [DataType(DataType.Date)]
+    public DateTime ReleaseDate{get;set;}
+    public string? Genre{get;set;}
+    public decimal Price{get;set;}
+}
